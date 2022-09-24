24ed95876f3aa0f633faf04b52b6d069b6fd24e7
Sohancod21 <sreecharansohan@gmail.com>
2022-09-25 00:27:12 +0530

    darktheme

---
+++ index.js
@@ -0,0 +1,6 @@
+
+        
+       document.getElementById("on").addEventListener("click", function mycss()
+       {
+        document.getElementById("css").setAttribute("href","darkstyle.css");
+       });
