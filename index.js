81938e7051c8e733e5be047e59a4a9f123e31a6a
Sohancod21 <sreecharansohan@gmail.com>
2022-09-27 13:43:16 +0530

    update

--- index.js
+++
@@ -1,6 +0,0 @@
-
-        
-       document.getElementById("on").addEventListener("click", function mycss()
-       {
-        document.getElementById("css").setAttribute("href","darkstyle.css");
-       });
